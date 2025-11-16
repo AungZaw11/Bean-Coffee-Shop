dc862a122ae05f646e6c3b7a3ec969c42e3a6447
AungZaw11 <azawmyint721@gmail.com>
2025-11-16 10:54:23 +0630

    Initial commit

---
+++ Coffee/Desgin.js
@@ -0,0 +1,75 @@
+// Cart
+
+function showPaymentTable() 
+{
+	document.getElementById('PaymentTable').style.visibility="visible";
+
+}
+
+function hidePaymentTable() 
+{
+	document.getElementById('PaymentTable').style.visibility="hidden";
+
+}
+
+function showmessage()
+ {
+ 	alert ("Payment Successfully");
+
+ }
+// Select
+ function GetCoffee() 
+{
+	document.getElementById('buyButton').style.visibility="visible";
+
+}
+
+function GetCoffee() 
+{
+	document.getElementById('buyButton').style.visibility="hidden";
+
+}
+
+function showCoffee()
+ {
+ 	alert ("Order Successfully");
+
+ }
+
+// GetRequirement
+ function GetRequirement() 
+{
+	document.getElementById('Tools').style.visibility="visible";
+
+}
+
+function GetRequirement() 
+{
+	document.getElementById('Tools').style.visibility="hidden";
+
+}
+
+function GetRequirementmessage()
+ {
+ 	alert ("Order Successfully For Requirement");
+
+ }
+
+// offer
+ function Login() 
+{
+	document.getElementById('Sub').style.visibility="visible";
+
+}
+
+function Login() 
+{
+	document.getElementById('Sub').style.visibility="hidden";
+
+}
+
+function LoginMessage()
+ {
+ 	alert ("Please First Login & Register");
+
+ }
